c47aedd1db40a5d9e96550a016869554deb98c2b
Sébastien Bonami <sbonami@ferlab.bio>
2023-03-24 11:39:38 -0400

    feat: INFRA-295 set version of each provider to keep stability

--- versions.tf
+++ versions.tf
@@ -1,7 +1,16 @@
 terraform {
   required_providers {
     openstack = {
-      source = "terraform-provider-openstack/openstack"
+      source  = "terraform-provider-openstack/openstack"
+      version = ">= 1.0.0, < 2.0.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "= 2.2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "= 3.4.0"
     }
   }
   required_version = ">= 0.14"
