981824cb9d06ad0957006443b3a3cbdcad54cb40
Eric Vallee <eric_vallee@webificservices.com>
2023-07-27 18:38:10 -0400

    feat: INFRA-403 Added major version restriction for cloudinit provider

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = ">= 1.0.0, < 2.0.0"
     }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "= 2.0.0, < 3.0.0"
+    }
   }
   required_version = ">= 1.2.0"
 }
